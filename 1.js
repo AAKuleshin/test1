2c6bf32c6f79dd81c71f1264f9ba44b5d91022b3
AAKuleshin <aakuleshin@yandex.ru>
2023-07-25 10:40:14 +0300

    First one

--- 1.js
+++ 1.js
@@ -1,1 +1,5 @@
-console.log("Hello")
+console.log("Hellonmp")
+let vars=111
+const res = typeof vars === undefined
+let data = (1,2,3)
+console.log(data.isArray())
---
+++ 2.js
@@ -0,0 +1,20 @@
+const logger = (param) => {
+    const colors = {
+        warning: '\x1b[1;33m',
+        error: '\x1b[0;31m',
+        info: '\x1b[1;37m',
+    }
+    const color = colors[param] || colors.info
+    return (str) => {
+        const date = new Date().toISOString()
+        console.log(color + date + `\t` + str)
+    }
+}
+
+const warning = logger('warning')
+const error = logger('error')
+const info = logger('info')
+
+warning('WARNINGGGGG')
+error('THIS IS ERROR')
+info('IT`s TEXT')
