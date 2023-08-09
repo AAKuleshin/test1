b02ca18b252e2f96e1256740506df6e12ffe2eb2
AAKuleshin <aakuleshin@yandex.ru>
2023-08-09 17:08:44 +0300

    Домашнее задание №2

--- scripts.js
+++ scripts.js
@@ -16,6 +16,32 @@ function logMapElements(value,key,map) {
     console.log(`map.get('${key}') = ${value}`)
 }
 
+function getMedium(ar) {
+    var sum = 0;
+    ar.forEach((v)=> {
+        sum += v
+        //console.log(v)
+    })
+    return sum/ar.length
+}
+
+
+Array.prototype.unique = function () {
+    var a = [];
+    var l = this.length;
+
+    for (var i = 0; i < l; i++) {
+        for (var j = i + 1; j < l; j++) {
+            if (this[i] === this[j]) {
+                j = ++i;
+            }
+        }
+        a.push(this[i]);
+    }
+
+    return a;
+};
+
 const div = document.getElementById('meteo');
 const li = document.getElementById('meteo');
 //const url = 'https://randomuser.me/api/?results=2'
@@ -34,7 +60,7 @@ fetch(API_URL_GEO_DATA)
         .then((resp) => resp.json())
         .then(function(data) {
             let coordinates = data //.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos;
-            console.log(data);
+            //console.log(data);
             let span = createNode('span')
             let results = data.hourly
             //Map.results.forEach(logMapElements)
@@ -44,6 +70,9 @@ fetch(API_URL_GEO_DATA)
                 span.innerHTML = `${res1} время : ${data.hourly_units.time} количество частиц pm10 : ${data.hourly_units.pm10} количество частиц pm2_5: ${data.hourly_units.pm2_5}`
             
             })*/
+            var pm10array = []
+            var pm2_5array = []
+            var datesarray = []
             for(let i=0;i<counts;i++) {
                 let span = createNode('span')
                 let spanText = createNode('span')
@@ -51,10 +80,55 @@ fetch(API_URL_GEO_DATA)
                 span.innerHTML = `${i+1} время : <b>${results.time[i]}</b> количество частиц pm10 : ${results.pm10[i]} количество частиц pm2_5: ${results.pm2_5[i]} <br>`
                 append(li,span)
                
+               date = results.time[i].split("T");
+               if(pm10array[date[0]] == undefined) {
+                    pm10array[date[0]] = []
+                    pm2_5array[date[0]] = []
+               }
+               datesarray.push(date[0])
+               pm10array[date[0]].push(results.pm10[i]);
+               pm2_5array[date[0]].push(results.pm2_5[i]);
+
+               
             }
+                        
+            dates = datesarray.unique();
+            pm2_5mediums = []
+            pm10mediums = []
+            dates.forEach((d) => {                
+                pm2_5mediums.push(getMedium(pm2_5array[d])) 
+                pm10mediums.push(getMedium(pm10array[d]))                
+            })
+
+
+            const ctx = document.getElementById('myChart');
+
+                new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: dates, //['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: 'pm2_5',
+                        data: pm2_5mediums,
+                        
+                        borderWidth: 1
+                    }, {
+                        label: 'pm10',
+                        data: pm10mediums,
+                        
+                        borderWidth: 2
+                    }]
+                },
+                options: {
+                    scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                    }
+                }
+                });
+            //console.log(pm10mediums)
             
-
-
         })
             
    }
